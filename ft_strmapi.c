7880b9f47a75b2609baf1f5c669570d3e6004099
Grace Haddad <gracehadda@gmail.com>
2025-06-14 12:33:45 +0000

    main

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -1,33 +1,55 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strmapi.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: grhaddad <grhaddad@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/04 16:23:37 by grhaddad          #+#    #+#             */
-/*   Updated: 2025/06/04 16:23:37 by grhaddad         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <stdlib.h>
 
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+size_t	ft_strlen(const char *s)
 {
-	char			*result;
+	size_t	len = 0;
+	while (s[len])
+		len++;
+	return (len);
+}
+
+char *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char	*new_str;
+	size_t	len;
 	unsigned int	i;
 
 	if (!s || !f)
 		return (NULL);
-	result = (char *)malloc(sizeof(char) * (strlen(s) + 1));
-	if (!result)
+	len = ft_strlen(s);
+	new_str = (char *)malloc((len + 1) * sizeof(char)); 
+	if (!new_str)
 		return (NULL);
 	i = 0;
-	while (s[i])
+	while (i < len)
 	{
-		result[i] = f(i, s[i]);
+		new_str[i] = f(i, s[i]);
 		i++;
 	}
-	result[i] = '\0';
-	return (result);
+	new_str[len] = '\0';
+	return (new_str);
 }
+
+
+#include <stdio.h>
+
+char to_upper(unsigned int i, char c) {
+	(void)i;
+	if (c >= 'a' && c <= 'z')
+		return (c - 32);
+	return (c);
+}
+
+int main()
+{
+	char *original = "hello, world!";
+	char *new_str = ft_strmapi(original, to_upper);
+	if (new_str)
+	{
+		printf("%s\n", new_str);
+		free(new_str);
+	}
+	return 0;
+}
+
+ 
